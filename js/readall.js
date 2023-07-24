41cece1baddf6e8bcecc1e96c48351151dfc5bcc
Akaruigo <51769012+Akaruigo@users.noreply.github.com>
2023-07-24 19:24:50 +0800

    Update readall.js

--- js/readall.js
+++ js/readall.js
@@ -47,7 +47,7 @@ function setupPlayer(containerId, videoSrc) {
 // 生成视频标题
 function generateVideoTitle(urlnum) {
     var timestamp = parseInt(urlnum);
-    var date = new Date(timestamp * 1000); // 转换为毫秒级时间戳
+    var date = new Date(( timestamp + 8 * 3600）* 1000); // 转换为毫秒级时间戳
     var formattedDate = date.toISOString().substring(0, 19).replace("T", " "); // 将ISO日期格式转换为指定格式
     return urlnum + " # " + formattedDate;
 }
